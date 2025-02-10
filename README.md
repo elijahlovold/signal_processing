ed655a050ba793b74b0c37c3c9e220ef425e2b1f
elovold <elovold@cadenceneuro.com>
2025-02-10 01:22:33 -0800

    improve example, add screenshots

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ make
 download fftw3 and the libfftw3-3.dll 
 
 you also need the to include the python <matplotlibcpp.h> and numpy core for plotting features 
+
+## Screenshot - 2nd Order Butterworth LPF - N = 2, Fs = 8kHz, Fc = 100Hz
+![Transfer Function](pictures/Figure_0.png)
+![Impulse Response](pictures/Figure_1.png)
+![Filter Result of 1Hz + 100Hz Sine](pictures/Figure_2.png)
--- examples.cpp
+++ examples.cpp
@@ -12,52 +12,75 @@ namespace plt = matplotlibcpp;
 namespace tf = transfer_functions;
 
 int main() {
-    // auto ab_pair = tf::butter(6, 5);
-    // std::vector<double> a = ab_pair.first;
-    // std::vector<double> b = ab_pair.second;
-
-    // tf::plot_ht(a, b, 40);
-
-
     // Set the size of the input array
     const int n = SAMPLE_RATE*5;
 
     vector<double> x_plot(n);
     vector<double> y_plot(n);
 
-    fftw_complex x[n];
-    fftw_complex x_win[n];
-    fftw_complex y[n];
+    // fftw_complex x[n];
+    // fftw_complex x_win[n];
+    // fftw_complex y[n];
+
+    vector<double> x(n);
+    vector<double> y(n);
 
     for (int i = 0; i < n; ++i) {
-        x[i][REAL] = sin(2.0*M_PI*i/SAMPLE_RATE) + sin(100*2.0*M_PI*i/SAMPLE_RATE); 
-        // x[i][REAL] = sin(M_PI*(i-n/2)/SAMPLE_RATE)/(M_PI*(i-n/2)/SAMPLE_RATE); 
-        x[i][IMAG] = 0;
+        x[i] = sin(2.0*M_PI*i/SAMPLE_RATE) + sin(100*2.0*M_PI*i/SAMPLE_RATE); 
+        // x[i][REAL] = sin(2.0*M_PI*i/SAMPLE_RATE) + sin(100*2.0*M_PI*i/SAMPLE_RATE); 
+        // x[i][IMAG] = 0;
     }
 
-    for (int i = 0; i < n; ++i) {
-        x_win[i][REAL] = x[i][REAL] * blk_harris(i, n); 
-        x_win[i][IMAG] = 0;
-    }
+    std::vector<double> a = {1, 2, 1};
+    std::vector<double> b = {684.8, -1293.6, 612.8};
 
-    fftw_plan plan = fftw_plan_dft_1d(n, x_win, y, FFTW_FORWARD, FFTW_ESTIMATE);
-    fftw_execute(plan);
-    fftw_destroy_plan(plan);
+    tf::print_TF(a, b);
+    tf::plot_TF(a, b);
+    tf::plot_ht(a, b, 500);
 
-    for (int i = 0; i < n; ++i) {
-        x_plot[i] = x[i][REAL];
-        y_plot[i] = cpx_abs(y, i);
+    y = tf::filt(a, b, x);
+
+    int N = x.size();
+
+    std::vector<double> ind(N);
+
+    for (double i = 0; i < N; i++){
+        ind[i] = i/SAMPLE_RATE;
     }
 
     plt::figure();
-    plt::plot(x_plot);
-
-    plt::figure();
-    plt::plot(y_plot);
+    plt::plot(ind, x);
+    plt::plot(ind, y);
+    plt::title("t-sig");
+    plt::xlabel("Time (s)");
+    plt::ylabel("Magnitude");
+    plt::grid(true);
 
     plt::show();
 
-    return 0;
+    // for (int i = 0; i < n; ++i) {
+    //     x_win[i][REAL] = x[i][REAL] * blk_harris(i, n); 
+    //     x_win[i][IMAG] = 0;
+    // }
+
+    // fftw_plan plan = fftw_plan_dft_1d(n, x_win, y, FFTW_FORWARD, FFTW_ESTIMATE);
+    // fftw_execute(plan);
+    // fftw_destroy_plan(plan);
+
+    // for (int i = 0; i < n; ++i) {
+    //     x_plot[i] = x[i][REAL];
+    //     y_plot[i] = cpx_abs(y, i);
+    // }
+
+    // plt::figure();
+    // plt::plot(x_plot);
+
+    // plt::figure();
+    // plt::plot(y_plot);
+
+    // plt::show();
+
+    // return 0;
 
 
 }
